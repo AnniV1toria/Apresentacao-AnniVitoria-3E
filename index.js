3dccfa7e83012bc81b50747e2dd8fe6219ea594a
AnniV1toria <163931772+AnniV1toria@users.noreply.github.com>
2024-09-03 23:06:24 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const primeiroNome = "Anni"
+const sobrenome = "Vitoria"
+const idade ="17"; 
+const cidadeNatal = "São José dos Pinhais"
+const altura = 1.68
+
+console.log("O meu primeiro nome é");
+console.log(primeiroNome);
+console.log("O meu segundo nome é");
+console.log(sobrenome);
+console.log("Minha idade é");
+console.log(idade);
+console.log("Minha cidade natal é");
+console.log(cidadeNatal);
+console.log("Minha altura é");
+console.log(altura);
